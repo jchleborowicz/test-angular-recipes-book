22617649627955454aa0ca15a195cb83b439eab0
Jacek Chleborowicz <j.chleborowicz@gmail.com>
2018-06-19 22:37:32 +0200

    Databinding.

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,16 +1,19 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, ElementRef, EventEmitter, Output, ViewChild} from '@angular/core';
+import {Ingredient} from '../../shared/ingredient.model';
 
 @Component({
   selector: 'app-shopping-edit',
   templateUrl: './shopping-edit.component.html',
   styleUrls: ['./shopping-edit.component.css']
 })
-export class ShoppingEditComponent implements OnInit {
+export class ShoppingEditComponent {
 
-  constructor() {
+  @Output() ingredientAdded = new EventEmitter<Ingredient>();
+  @ViewChild('nameInput') nameInput: ElementRef;
+  @ViewChild('amountInput') amountInput: ElementRef;
+
+  onIngredientAdded() {
+    this.ingredientAdded.emit(new Ingredient(this.nameInput.nativeElement.value,
+      this.amountInput.nativeElement.value));
   }
-
-  ngOnInit() {
-  }
-
 }
