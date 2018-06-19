db5fb291e4ccffd07291607db99dcb0fd3a13ce2
Jacek Chleborowicz <j.chleborowicz@gmail.com>
2018-06-19 20:38:45 +0200

    Databinding.

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,16 +1,15 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, EventEmitter, Output} from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
-export class HeaderComponent implements OnInit {
+export class HeaderComponent {
 
-  constructor() {
+  @Output() contentChange = new EventEmitter<string>();
+
+  onMenuItem(name: string) {
+    this.contentChange.emit(name);
   }
-
-  ngOnInit() {
-  }
-
 }
