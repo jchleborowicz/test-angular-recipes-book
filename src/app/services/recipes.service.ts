6389fd3347eb81de89fd077ab2684d26f7dc6539
Jacek Chleborowicz <j.chleborowicz@gmail.com>
2018-06-23 20:05:44 +0200

    Shopping list service.

--- src/app/services/recipes.service.ts
+++ src/app/services/recipes.service.ts
@@ -1,5 +1,6 @@
 import {EventEmitter, Injectable} from '@angular/core';
 import {Recipe} from '../shared/recipe.model';
+import {Ingredient} from '../shared/ingredient.model';
 
 @Injectable({
   providedIn: 'root'
@@ -10,9 +11,19 @@ export class RecipesService {
 
   private recipes: Recipe[] = [
     new Recipe('Hamburger', 'Very tasty hamburger',
-      'https://www.maxpixel.net/static/photo/640/Sandwich-Meal-Lunch-Hamburger-Cheeseburger-Food-31775.jpg'),
+      'https://www.maxpixel.net/static/photo/640/Sandwich-Meal-Lunch-Hamburger-Cheeseburger-Food-31775.jpg',
+      [
+        new Ingredient('Buns', 2),
+        new Ingredient('Meat', 1),
+        new Ingredient('French Fries', 20)
+      ]),
     new Recipe('Celery salad', 'It\'s healthy and tasty!!!',
-      'https://assets.bonappetit.com/photos/57ae36fc1b33404414975bc5/16:9/w_1280,c_limit/celery-salad-with-celery-root-and-horseradish.jpg')
+      'https://assets.bonappetit.com/photos/57ae36fc1b33404414975bc5/16:9/w_1280,c_limit/celery-salad-with-celery-root-and-horseradish.jpg',
+      [
+        new Ingredient('Celery', 2),
+        new Ingredient('Onion', 3),
+        new Ingredient('Carrot', 4)
+      ])
   ];
 
   constructor() {
