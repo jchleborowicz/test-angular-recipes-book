2fec4b7607779c9b5ada4e93ea34cf8f5c00416a
Jacek Chleborowicz <j.chleborowicz@gmail.com>
2018-06-23 17:32:53 +0200

    Recipe service.

--- src/app/services/shoping-list.service.ts
+++ src/app/services/shoping-list.service.ts
@@ -1,10 +1,27 @@
-import {Injectable} from '@angular/core';
+import {EventEmitter, Injectable} from '@angular/core';
+import {Ingredient} from '../shared/ingredient.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ShopingListService {
 
+  ingredientsChanged = new EventEmitter<void>();
+
+  ingredients: Ingredient[] = [
+    new Ingredient('Apples', 5),
+    new Ingredient('Tomatoes', 10)
+  ];
+
   constructor() {
+  }
+
+  addIngredient(ingredient: Ingredient): void {
+    this.ingredients.push(ingredient);
+    this.ingredientsChanged.emit();
+  }
+
+  getIngredients(): Ingredient[] {
+    return this.ingredients;
   }
 }
--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,19 +1,31 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {Ingredient} from '../shared/ingredient.model';
+import {ShopingListService} from '../services/shoping-list.service';
 
 @Component({
   selector: 'app-shopping-list',
   templateUrl: './shopping-list.component.html',
   styleUrls: ['./shopping-list.component.css']
 })
-export class ShoppingListComponent {
+export class ShoppingListComponent implements OnInit {
 
-  ingredients: Ingredient[] = [
-    new Ingredient('Apples', 5),
-    new Ingredient('Tomatoes', 10)
-  ];
+  ingredients: Ingredient[] = [];
+
+  constructor(private shoppingListService: ShopingListService) {
+  }
+
+  ngOnInit(): void {
+    this.reloadIngrediens();
+    this.shoppingListService.ingredientsChanged.subscribe(
+      () => this.reloadIngrediens()
+    );
+  }
+
+  private reloadIngrediens() {
+    this.ingredients = this.shoppingListService.getIngredients();
+  }
 
   onIngredientAdded(ingredient: Ingredient) {
-    this.ingredients.push(ingredient);
+    this.shoppingListService.addIngredient(ingredient);
   }
 }
